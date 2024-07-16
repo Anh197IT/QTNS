caa6bb0da00bb480c22b6ae02fc030ffdb04ac47
Anh197IT <hominhanh2212@gmail.com>
2024-07-16 18:37:02 +0700

    init QLNS

---
+++ ql nha sach/Models/LICHSUNHAPSACH.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ql_nha_sach.Models
+{
+    [Table("LichSuNhapSach")]
+    public partial class LichSuNhapSach /*khai báo một lớp partial tên là LichSuNhapSach*/
+    {
+        [Key] /*chỉ ra rằng thuộc tính Id là khóa chính của bảng.*/
+        public int Id { get; set; }
+
+        [Required] /*chỉ ra rằng MaSach là bắt buộc phải có giá trị.*/
+        [StringLength(20)] /*giới hạn độ dài tối đa của chuỗi là 20 ký tự.*/
+        public string MaSach { get; set; }
+
+        public DateTime NgayNhap { get; set; }
+
+        public int SoLuongNhap { get; set; }
+
+        [ForeignKey("MaSach")] /*chỉ ra rằng thuộc tính SACH là khóa ngoại liên kết với thuộc tính MaSach.*/
+        public virtual SACH SACH { get; set; }
+    }
+}
---
+++ ql nha sach/Models/Model1.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Caching;
+
+namespace ql_nha_sach.Models
+{    public partial class Model1 : DbContext
+    {
+        /*Đây là constructor của lớp Model1.
+        Nó gọi constructor của lớp cơ sở(DbContext) với tham số "name=Model13",
+        chỉ định tên của chuỗi kết nối được định nghĩa trong tệp cấu hình của ứng dụng
+        (thường là web.config hoặc app.config).*/
+        public Model1()
+            : base("name=Model13")
+        {
+        }
+
+        public virtual DbSet<KHACHHANG> KHACHHANGs { get; set; }
+        public virtual DbSet<LOAISACH> LOAISACHes { get; set; }
+        public virtual DbSet<SACH> SACHes { get; set; }
+        public virtual DbSet<TAIKHOAN> TAIKHOANs { get; set; }
+        /* public virtual DbSet<THEMUON> THEMUONs { get; set; }*/
+        public virtual DbSet<LichSuNhapSach> LichSuNhapSaches { get; set; } // Thêm dòng này
+        /*Những dòng này định nghĩa các thuộc tính kiểu DbSet<T>.
+            *Một DbSet đại diện cho một tập hợp các thực thể có thể được truy vấn
+            *từ cơ sở dữ liệu và có thể được cập nhật.
+            KHACHHANGs, LOAISACHes, SACHes, TAIKHOANs, và LichSuNhapSaches 
+        đại diện cho các bảng khác nhau trong cơ sở dữ liệu.*/
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            /* Cấu hình thực thể LOAISACH.*/
+            modelBuilder.Entity<LOAISACH>()
+                .HasMany(e => e.SACHes)
+                .WithOptional(e => e.LOAISACH)
+                .WillCascadeOnDelete();/*'WillCascadeOnDelete' đảm bảo rằng nếu một LOAISACH bị xóa,
+                                        * các SACH liên quan cũng sẽ bị xóa theo.*/
+
+
+            modelBuilder.Entity<TAIKHOAN>()
+                .Property(e => e.Matkhau)
+                .IsFixedLength()
+                .IsUnicode(false);
+
+
+        }
+    }
+}
